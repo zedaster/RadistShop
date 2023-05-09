010b74ea2c0349ab30720abb360bdb23595ab4f8
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2023-05-09 18:04:17 +0500

    Install bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <template>
+  <!--
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  -->
+
+  <div class="container">
+    <h1>Hello world!</h1>
+  </div>
+
+  <!--
+  <button class="btn btn-primary" data-bs-target="#collapseTarget" data-bs-toggle="collapse">
+    Bootstrap collapse
+  </button>
+  <div class="collapse py-2" id="collapseTarget">
+    This is the toggle-able content!
+  </div>
+  -->
 </template>
 
 <script lang="ts">
@@ -10,7 +25,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    //HelloWorld,
   },
 });
 </script>
