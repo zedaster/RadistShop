4e020ca18196eb4bd9f6549857a944e754c44330
Sergey Kazantsev <serzh.kazantseff@gmail.com>
2023-05-18 13:40:09 +0500

    Add filters UI

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,12 +2,59 @@
   <loading-container v-if="isLoading" />
   <section v-else>
     <div class="container px-4 px-lg-5 mt-5">
+      <div class="row pb-3 gy-2">
+        <div class="col-sm-12 col-md-3 col-lg-3 col-xl-2">
+          <select class="form-select" id="sortType">
+            <option selected value="default">По умолчанию</option>
+            <option value="alphabet">По алфавиту</option>
+            <option value="ascendingPrice">По возрастанию цены</option>
+            <option value="descendingPrice">По убыванию цены</option>
+          </select>
+        </div>
+        <div class="col-sm-12 col-md-6 col-lg-6 col-xl-8">
+          <input class="form-control" type="text" id="search" placeholder="Поиск">
+        </div>
+        <div class="col-sm-12 col-md-3 сol-lg-3 col-xl-2">
+          <button class="btn btn-outline-dark dropdown-toggle dropdown-filters-button" type="button"
+                  data-bs-toggle="dropdown" aria-expanded="false">Фильтры
+          </button>
+          <ul class="dropdown-menu dropdown-filters">
+            <h6 class="dropdown-header">Избранное</h6>
+            <li class="dropdown-item">
+              <div class="form-check form-switch">
+                <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault">
+                <label class="form-check-label" for="flexSwitchCheckDefault">Показать избранное</label>
+              </div>
+            </li>
+            <li>
+              <hr class="dropdown-divider" />
+            </li>
+            <h6 class="dropdown-header">Категории</h6>
+            <li class="dropdown-item">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                  men's clothing
+                </label>
+              </div>
+            </li>
+            <li class="dropdown-item">
+              <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                <label class="form-check-label" for="flexCheckDefault">
+                  women's clothing
+                </label>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </div>
       <div
         class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
       >
         <product-card v-for="product in products"
                       :key="product.id"
-          :product="product" :cart="cart"
+                      :product="product" :cart="cart"
         />
       </div>
     </div>
@@ -27,21 +74,21 @@ export default defineComponent({
   components: {
     //HelloWorld,
     LoadingContainer,
-    ProductCard,
+    ProductCard
   },
   props: {
     cart: {
       type: Object as PropType<Cart>,
-      required: true,
+      required: true
     }
   },
   data() {
     return {
       isLoading: true,
-      products: null as Array<Product> | null,
-    }
+      products: null as Array<Product> | null
+    };
   },
-  created() : void {
+  created(): void {
     Products
       .loadAllProducts()
       .then((products) => {
@@ -52,7 +99,15 @@ export default defineComponent({
 });
 </script>
 
-<style>
-#app {
+<style scoped>
+.search-input {
+  width: 100%;
+}
+
+#sortType {
+}
+
+.dropdown-filters-button {
+  width: 100%;
 }
 </style>
